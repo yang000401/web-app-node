cbc3ac4b68948ab8c8d7acf2064f34e4e5cd449a
yang000401 <87750073+yang000401@users.noreply.github.com>
2022-05-26 14:00:08 +0900

    옛날 강의
    옛날 강의 여서 var를 사용하기도 하길래 조금 바꿈

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var http = require("http");
-var fs = require("fs");
-var app = http.createServer(function (request, response) {
-  var url = request.url;
+let http = require("http");
+let fs = require("fs");
+let app = http.createServer(function (request, response) {
+  let url = request.url;
   if (request.url == "/") {
     url = "/index.html";
   }
@@ -11,7 +11,6 @@ var app = http.createServer(function (request, response) {
     return;
   }
   response.writeHead(200);
-  console.log(__dirname + url);
   response.end(fs.readFileSync(__dirname + url));
 });
 app.listen(3000);
