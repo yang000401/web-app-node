8099f0f726e39751791e97d0f320ce6a525cb3a5
yang000401 <87750073+yang000401@users.noreply.github.com>
2022-05-26 14:06:11 +0900

    찝찝해서 바꿈
    언잖아서 바꿈

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let http = require("http");
-let fs = require("fs");
-let app = http.createServer(function (request, response) {
-  let url = request.url;
+const http = require("http");
+const fs = require("fs");
+const app = http.createServer(function (request, response) {
+  const url = request.url;
   if (request.url == "/") {
     url = "/index.html";
   }
